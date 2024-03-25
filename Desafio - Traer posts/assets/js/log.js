4db2fda30f3bd130111ed829c07e220f19b5ba4c
pupubeat <ratenas95@gmail.com>
2024-03-24 21:53:27 -0300

    desafio js traer posts

---
+++ Desafio - Traer posts/assets/js/log.js
@@ -0,0 +1,22 @@
+// Extraer elementos del DOM.
+const postData = document.querySelector("#post-data")
+const traerPosts = document.querySelector("#btnPosts")
+
+// Obtener data de la AÍ a través de fetch
+const getPosts = async () => {
+
+    // Manejar los posibles errores con try-catch
+    try {
+        const apiResponse = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const apiData = await apiResponse.json()
+        apiData.forEach((item) => {
+            postData.innerHTML += `
+            <li>${item.title}</li>
+            <p>${item.body}</p>
+            `
+        })
+    }
+    catch (error) {
+        console.log(error.message)
+    }
+}
