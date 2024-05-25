2b642f495f490cc4a5b176e6cf3b79f2388a621d
pupubeat <ratenas95@gmail.com>
2024-05-25 01:26:29 -0400

    actualizacion routers prueba M6

--- Desafio - Club Deportivo/controllers/deportes.controller.js
+++ Desafio - Club Deportivo/controllers/deportes.controller.js
@@ -1,7 +1,50 @@
-export const
+// Función para agregar un nuevo deporte.
+export const agregar = async (req, res) => {
+    try {
+        const { nombre, precio } = req.query
+        console.log(req.query) // Comprobar los datos en req.query, pasarlos por consola.
+        const nuevoDeporte = pass
+        return res.status(201).json(nuevoDeporte)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ ok: false })
+    }
+}
 
+// Función para devolver en formato JSON todos los deportes.
+export const getData = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ ok: false })
+    }
+}
+
+// Función para editar el precio de un deporte en específico.
+export const edit = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ ok: false })
+    }
+}
+
+// Función para eliminar un deporte en específico.
+export const eliminar = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ ok: false })
+    }
+}
 
 // Exportar los controllers de Deportes.
 export const deportesController = {
-
+    agregar,
+    getData,
+    edit,
+    eliminar
 }
--- Desafio - Club Deportivo/routers/deportes.routers.js
+++ Desafio - Club Deportivo/routers/deportes.routers.js
@@ -4,13 +4,19 @@ import { deportesController } from "../controllers/deportes.controller.js";
 const router = Router()
 
 // Crear una ruta que reciba el nombre y precio de un nuevo deporte, lo persista en un archivo JSON.
-router.post('/agregar', deportesController)
+// PATH /deportes/agregar
+router.post('/agregar', deportesController.agregar)
 
 // Crear una ruta que al consultarse devuelva en formato JSON todos los deportes registrados.
-router.get('/', deportesController)
+// PATH /deportes
+router.get('/', deportesController.getData)
 
 // Crear una ruta que edite el precio de un deporte registrado utilizando los parámetros de la consulta y persista este cambio.
-router.put('/editar', deportesController)
+// PATH /deportes/editar
+router.put('/editar', deportesController.edit)
 
 // Crear una ruta que elimine un deporte solicitado desde el cliente y persista este cambio.
-router.delete('/eliminar', deportesController)
+// PATH /deportes/eliminar
+router.delete('/eliminar', deportesController.eliminar)
+
+export default router
