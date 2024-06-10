275b2032ff800e857dfccc6331d2f30d47afeeff
pupubeat <ratenas95@gmail.com>
2024-06-10 01:48:29 -0400

    primer commit: creacion carpeta y archivo para routers

---
+++ Desafio - FBI System/data/agentes.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    email: 'who@fbi.com',
+    password: 'me',
+  },
+  {
+    email: 'where@fbi.com',
+    password: 'there',
+  },
+  {
+    email: 'how@fbi.com',
+    password: 'exactly',
+  },
+]
---
+++ Desafio - FBI System/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from 'express'
+import agentesRouter from './routers/agentes.router.js'
+
+// Archivos estáticos públicos.
+app.use(express.static('public'))
+
+// Middlewares para activar el req.body
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Conectar al puerto 3000 o a uno en específico.
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Example app listening http://localhost:${PORT}`)
+})
