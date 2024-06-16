3fa9cd493b1e4afd19d671415699198748a5cf5d
pupubeat <ratenas95@gmail.com>
2024-06-16 17:54:27 -0400

    archivo router canciones modificado y listo

---
+++ Desafio - Mi Repertorio/controllers/canciones.controller.js
@@ -0,0 +1,22 @@
+const addCancion = async (req, res) => {
+
+}
+
+const getAllCanciones = async (req, res) => {
+
+}
+
+const updateCancion = async (req, res) => {
+
+}
+
+const removeCancion = async (req, res) => {
+
+}
+
+export const cancionesControllers = {
+    addCancion,
+    getAllCanciones,
+    updateCancion,
+    removeCancion
+}
--- Desafio - Mi Repertorio/routers/canciones.router.js
+++ Desafio - Mi Repertorio/routers/canciones.router.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
+import { cancionesControllers } from "../controllers/canciones.controller.js";
 
 const router = Router()
 
 // Crear una ruta que reciba los datos correspondientes a una canción y realice a través de una función asíncrona la inserción en la tabla canciones.
-router.post('/cancion',)
+router.post('/cancion', cancionesControllers.addCancion)
 
 // Crear una ruta que devuelva un JSON con los registros de la tabla canciones.
-router.get('',)
+router.get('', cancionesControllers.getAllCanciones)
 
 // Crear una ruta que reciba los datos de una canción que se desea editar
-router.put('',)
+router.put('/cancion', cancionesControllers.updateCancion)
 
 // Crear una ruta que reciba por queryString el id de una canción y realiza una consulta SQL a través de una función asíncrona para eliminarla de la base de datos.
-router.delete('/cancion',)
+router.delete('/cancion', cancionesControllers.removeCancion)
 
 export default router
