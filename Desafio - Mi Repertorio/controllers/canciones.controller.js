ef1fc9696dede3596483e5d2e65fdfbb459d52fc
pupubeat <ratenas95@gmail.com>
2024-06-16 19:13:22 -0400

    actualizados controllers y models

--- Desafio - Mi Repertorio/controllers/canciones.controller.js
+++ Desafio - Mi Repertorio/controllers/canciones.controller.js
@@ -1,42 +1,60 @@
 import { cancionesModels } from "../models/canciones.model.js"
 
+// Controller que recibe los datos correspondientes a una canción y realice a través de una función asíncrona la inserción en la tabla canciones.
 const addCancion = async (req, res) => {
     try {
-
+        const { titulo, artista, tono } = req.body
+        console.log(req.body)
+        const cancion = await cancionesModels.register({ titulo, artista, tono })
+        return res.status(201).json(cancion)
     } catch (error) {
-        console.log(error)
+        console.log(error) // Capturar error en consola
         res.status(500).json({ ok: false })
     }
 }
 
+// Controller que devuelve un JSON con los registros de la tabla canciones.
 const getAllCanciones = async (req, res) => {
     try {
         const canciones = await cancionesModels.findAll()
         return res.json(canciones)
+
     } catch (error) {
-        console.log(error)
+        console.log(error) // Capturar error en consola
         res.status(500).json({ ok: false })
     }
 }
 
+// Controller que recibe los datos de una canción que se desea editar.
 const updateCancion = async (req, res) => {
     try {
+        const { id } = req.query
+        console.log(req.query)
+        const { titulo, artista, tono } = req.body
+        console.log(req.body)
+        const cancion = await cancionesModels.edit({ id, titulo, artista, tono })
+        return res.json(cancion)
 
     } catch (error) {
-        console.log(error)
+        console.log(error) // Capturar error en consola
         res.status(500).json({ ok: false })
     }
 }
 
+// Controller que recibe por queryString el id de una canción y realiza una consulta SQL a través de una función asíncrona para eliminarla de la base de datos.
 const removeCancion = async (req, res) => {
     try {
+        const { id } = req.query
+        const cancion = await cancionesModels.remove({ id })
+        return res.json(cancion)
 
     } catch (error) {
-        console.log(error)
+        console.log(error) // Capturar error en consola
         res.status(500).json({ ok: false })
     }
 }
 
+// Exportar los controllers.
 export const cancionesControllers = {
     addCancion,
     getAllCanciones,
--- Desafio - Mi Repertorio/models/canciones.model.js
+++ Desafio - Mi Repertorio/models/canciones.model.js
@@ -1,5 +1,21 @@
 import { pool } from '../database/connection.db.js'
 
+// Model para registrar una canción en la Database.
+const register = async ({ titulo, artista, tono }) => {
+    const query = {
+        text: `
+        INSERT INTO CANCIONES
+        (TITULO, ARTISTA, TONO])
+        VALUES ($1, $2, $3)
+        RETURNING *
+        `,
+        values: [titulo, artista, tono],
+        rowMode: "array"
+    }
+    const { rows } = await pool.query(query)
+    return rows[0]
+}
+// Model para obtener la data de todas las canciones registradas en la Database.
 const findAll = async () => {
     const query = {
         text: `
@@ -11,15 +27,42 @@ const findAll = async () => {
     return rows
 }
 
-const query = {
-    text: `
-    SELECT * FROM ESTUDIANTES
-    `,
-    rowMode: "array"
+// Model que edita la data de una canción desde la Database.
+const edit = async ({ id, titulo, artista, tono }) => {
+    const query = {
+        text: `
+            UPDATE CANCIONES
+            SET TITULO = $2,
+            ARTISTA = $3,
+            TONO = $4
+            WHERE ID = $1
+        `,
+        values: [id, titulo, artista, tono],
+        rowMode: "array"
+    }
+    const { rows } = await pool.query(query)
+    return rows[0]
 }
-const { rows } = await pool.query(query)
-return rows
 
+// Model que elimina una canción desde la Database.
+const remove = async ({ id }) => {
+    const query = {
+        text: `
+        DELETE FROM CANCIONES
+        WHERE ID = $1
+        RETURNING *
+        `,
+        values: [id],
+        rowMode: "array"
+    }
+    const { rows } = await pool.query(query)
+    return rows[0]
+}
+
+// Exportar los models.
 export const cancionesModels = {
-    findAll
+    register,
+    findAll,
+    edit,
+    remove
 }
