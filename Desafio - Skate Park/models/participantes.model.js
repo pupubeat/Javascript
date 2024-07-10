1a33dbfcbbb430e81e10435fd6b1534cb45083cb
pupubeat <ratenas95@gmail.com>
2024-07-09 23:42:15 -0400

    participantes models register actualizado

--- Desafio - Skate Park/models/participantes.model.js
+++ Desafio - Skate Park/models/participantes.model.js
@@ -1,3 +1,19 @@
+import { pool } from '../database/connection.db.js'
+
+export const register = async ({ email, nombre, password, anos_experiencia, especialidad, foto, estado }) => {
+    const query = {
+        text:
+            `INSERT INTO skaters
+            (email, nombre, password, anos_experiencia, especialidad, foto, estado)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            RETURNING *
+            `,
+        values: [email, nombre, password, anos_experiencia, especialidad, foto, estado]
+    }
+    const rows = await pool.query(query)
+    return rows[0]
+}
+
 export const skaterModels = {
     register,
     login,
