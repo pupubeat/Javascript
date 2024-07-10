ce2082f9fa781a9620312ac6a189f50fcf3b8bca
pupubeat <ratenas95@gmail.com>
2024-07-09 22:08:31 -0400

    participantes controllers actualizado

--- Desafio - Skate Park/controllers/participantes.controller.js
+++ Desafio - Skate Park/controllers/participantes.controller.js
@@ -1,3 +1,32 @@
-export const skatersControllers = {
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { skaterModels } from "../models/participantes.model.js"
 
+export const register = async (req, res) => {
+    try {
+        const { email, nombre, anos_experiencia, especialidad, foto } = req.body
+        console.log(req.body)
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const nuevoSkater = await skaterModels.register({
+            email,
+            nombre,
+            password: hashedPassword,
+            anos_experiencia,
+            especialidad,
+            foto
+        })
+        res.status(201).json({ message: 'Usuario creado con éxito' })
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+    }
+}
+
+export const skatersControllers = {
+    register,
+    login,
+    getAllSkaters,
+    getOneSkater,
+    modifySkater,
+    removeSkater
 }
---
+++ Desafio - Skate Park/models/participantes.model.js
@@ -0,0 +1,3 @@
+export const skaterModels = {
+    register
+}
