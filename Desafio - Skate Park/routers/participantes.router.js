8608e2808b3865495801acafbebd04c23b55af8c
pupubeat <ratenas95@gmail.com>
2024-07-17 23:48:24 -0400

    cambios en participantes routers

--- Desafio - Skate Park/routers/participantes.router.js
+++ Desafio - Skate Park/routers/participantes.router.js
@@ -3,22 +3,27 @@ import { skatersControllers } from "../controllers/participantes.controller.js";
 
 const router = Router()
 
+// Login/Register
+
 // Ruta para registrar nuevos participantes.
 router.post('/register', skatersControllers.registerSkater)
-
 // Ruta para que los participantes puedan iniciar sesión con su correo y contraseña.
 router.post('/login', skatersControllers.loginSkater)
 
+// CRUD
+
 // Ruta para obtener a todos los participantes de la database.
 router.get('/', skatersControllers.getAllSkaters)
-
 // Ruta para obtener a un solo participante en específico de la database, a través de su email.
 router.get('/:email', skatersControllers.getOneSkater)
-
 // Ruta para modificar los datos de un participante.
 router.put('/:email', skatersControllers.modifySkater)
-
 // Ruta para eliminar los datos de un participante.
 router.delete('/:email', skatersControllers.removeSkater)
+
+// Perfiles de los skaters.
+router.get('/perfil',)
+
+// Administradores.
 
 export default router
